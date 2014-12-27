fa3ab56a80c05937b6dbbe296d0cebafc7237892
Mazelium <matthieu@ossete.fr>
2014-12-27 21:28:54 +0100

    Ajout img fond menu + musique écran titre

---
+++ Build/CMakeFiles/manguille.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/manguille.dir/src/manguille.cpp.o"
+  "bin/manguille.pdb"
+  "bin/manguille"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/manguille.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
